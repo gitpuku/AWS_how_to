ce0cf9f3daee1335a896ce17a6f1f8d3a5adfd71
tomomon <fujiwara.tomohiro456@gmail.com>
2025-10-20 22:52:19 +0900

    DAY5folder

---
+++ サーバーレス on AWS/DAY05/js/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+	API_BASE_URL: 'https://lz76u27qg1.execute-api.us-east-1.amazonaws.com', // API Gatway のデフォルトエンドポイントを入力
+  COGNITO: {
+    USER_POOL_ID: 'ap-northeast-1_XXXXXXXXX', // User Pool ID を入力
+    CLIENT_ID: 'XXXX',    // Client ID を入力
+    REGION: 'ap-northeast-1'
+  }
+};
+
+window.CONFIG = CONFIG;
